6b918ebd9b9be6aa624a1ec765e4ba77b9af8f79
kabuqinuofu <kabuqinuofu@gmail.com>
2017-04-21 17:03:23 +0800

    Initial commit

---
+++ app/src/main/java/tool/yc/com/xswitchbutton/MainActivity.java
@@ -0,0 +1,31 @@
+package tool.yc.com.xswitchbutton;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.CompoundButton;
+
+import tool.yc.com.library.SwitchButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SwitchButton mSwitchButtonView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mSwitchButtonView = (SwitchButton) findViewById(R.id.switch_button);
+        init();
+    }
+
+    private void init() {
+        mSwitchButtonView.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isCheck) {
+                Log.e("YC", "选中状态：" + isCheck);
+            }
+        });
+    }
+
+}
